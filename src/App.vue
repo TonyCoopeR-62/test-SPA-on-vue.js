152253937cb6a5733deb8827ad74923c89d7df4e
TonyCooper-62 <jar1991@mail.ru>
2019-09-23 12:55:27 +0300

    simple SPA on vue.js

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+<div>
+  <button class="btn btn-primary" v-on:click="showRes = !showRes">
+    {{ btnText }}
+  </button>
+  <hr>
+  <appProgress v-bind:val="sum" v-bind:max="maxNumbers * 5"></appProgress>
+  <transition name="slide">
+    <h2 v-show="showRes" class="alert alert-success">
+      Your profit: {{ sum }}</h2>
+  </transition>
+
+  <hr>
+
+  <appProgress v-bind:val="numbers.length" v-bind:max="maxNumbers"></appProgress>
+
+  <button class="btn btn-success" v-on:click="addNumber" v-bind:disabled="done">
+    Add number
+  </button>
+  <hr>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="number in numbers" v-bind:key="number"> {{ number }} </li>
+  </ul>
+</div>
+</template>
+<script>
+import progress from './progress.vue';
+export default {
+  data() {
+    return {
+      showRes: true,
+      numbers: [],
+      maxNumbers: 10
+    }
+  },
+  methods: {
+    addNumber() {
+      if (!this.done) {
+        let rnd = Math.floor(Math.random() * 11) - 5;
+        this.numbers.push(rnd);
+      }
+    }
+  },
+  computed: {
+    sum() {
+      let sum = 0
+      for(let i = 0; i < this.numbers.length; i++) {
+        sum += this.numbers[i];
+      }
+      return sum;
+    },
+    btnText() {
+      return this.showH2 ? 'Hide result' : 'Show result';
+    },
+    done() {
+      return this.numbers.length >= this.maxNumbers;
+    }
+  },
+  components: {
+    AppProgress: progress
+  }
+}
+</script>
+<style scoped>
+  .slide-enter {
+    opacity: 0;
+  }
+
+  .slide-enter-active {
+    transition: opacity 0.5s;
+  }
+
+  .slide-leave-active {
+    transition: opacity 0.5s;
+  }
+
+  .slide-leave-to {
+    opacity: 0;
+  }
+</style>
